f649ea80a3299ecf8a214e801b51aec3e3cdaa5e
Elvis Muchasaj <elvis.muchasaj@gmail.com>
2020-09-10 19:00:59 +0200

    Added Tags and BlogPostTags Table seeder

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Tag;
+use Illuminate\Database\Seeder;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = collect([
+            'Science', 
+            'Sports', 
+            'Politics', 
+            'Entertainment', 
+            'Economy', 
+            'Natyre', 
+            'Movies', 
+            'Drama', 
+            'World'
+        ]);
+
+        $tags->each(function($tagName) {
+            $tag = new Tag();
+            $tag->name = $tagName;
+            $tag->save();
+        });
+
+    }
+}
