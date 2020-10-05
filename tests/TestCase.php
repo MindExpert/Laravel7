0368a628d3724da0b78e020f08611badb5113e76
Elvis Muchasaj <elvis.muchasaj@gmail.com>
2020-10-05 18:09:00 +0200

    Added a test ApiTest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use App\User;
+use App\BlogPost;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -12,6 +13,13 @@ abstract class TestCase extends BaseTestCase
         protected function user()
         {
             return factory(User::class)->create();
+        }
+
+        public function blogPost()
+        {
+            return factory(BlogPost::class)->create([
+                'user_id' => $this->user()->id
+            ]);
         }
 
 }
